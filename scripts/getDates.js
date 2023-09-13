82f206d036e0aa3096686a501bee677967a55593
peter <peter@DT_PETER>
2023-09-13 06:58:23 +0200

    Updated

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,2 @@
-document.querySelector("#year").textContent = new Date().getFullYear();
-document.querySelector("#lastModified").textContent = `Last Modification:  ${document.lastModified}`;
+document.querySelector("#year").innerHTML = new Date().getFullYear();
+document.querySelector("#lastModified").innerHTML = document.lastModified;
