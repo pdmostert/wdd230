c39148eea0fdddacbf57ef1df7a774f4e81f675d
peter <peter@DT_PETER>
2023-10-11 18:36:05 +0200

    Group Work Prep

---
+++ lesson05/scripts/main.js
@@ -0,0 +1,20 @@
+const list = document.querySelector("#list");
+const button = document.querySelector("button");
+const input = document.querySelector("#favchap");
+
+button.addEventListener("click", () => {
+  if (input.value != "") {
+    const listItem = document.createElement("li");
+    const btn = document.createElement("button");
+    listItem.textContent = input.value;
+    btn.textContent = "❌";
+    btn.addEventListener("click", () => {
+      listItem.remove();
+      input.focus();
+    });
+    listItem.appendChild(btn);
+    list.appendChild(listItem);
+  }
+  input.focus();
+  input.value = "";
+});
