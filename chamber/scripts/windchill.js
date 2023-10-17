cffc467045295e6f4f4bc92ed27b53c5014dcff4
peter <peter@DT_PETER>
2023-10-17 18:01:01 +0200

    Updated

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,18 @@
+let tempF = parseFloat(document.getElementById("temperature").textContent);
+let speed = parseFloat(document.getElementById("wind-speed").textContent);
+
+function CalculateWindChill(tempF, speed) {
+  if (tempF > 10 || speed < 4.8) return "N/A";
+
+  let chill =
+    35.74 +
+    0.6215 * tempF -
+    35.75 * speed ** 0.16 +
+    0.4275 * tempF * speed ** 0.16;
+  return chill.toFixed(2);
+}
+
+document.getElementById("wind-chill").textContent = CalculateWindChill(
+  tempF,
+  speed
+);
