f0f807b3cdcc3292463d9547073343bebbc32210
peter <peter@DT_PETER>
2023-11-01 21:06:31 +0200

    Form Page

---
+++ scripts/form.js
@@ -0,0 +1,31 @@
+const pass1 = document.querySelector("#key");
+const pass2 = document.querySelector("#key-repeat");
+const email = document.querySelector("#email-input");
+const message = document.querySelector("#message");
+
+pass2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+  if (pass1.value !== pass2.value) {
+    pass2.style.backgroundColor = "rgb(201, 120, 120)";
+    message.innerHTML = "Passwords don't match";
+    pass2.value = "";
+  } else {
+    pass2.style.backgroundColor = "rgb(173, 229, 173)";
+    message.innerHTML = "";
+  }
+}
+
+email.addEventListener("focusout", validateEmail);
+
+function validateEmail() {
+  var email = document.getElementById("email-input").value;
+  var pattern = /^[a-zA-Z0-9._%+-]+@byui\.edu$/;
+  if (pattern.test(email)) {
+    email.style.backgroundColor = "rgb(173, 229, 173)";
+  } else {
+    message.innerHTML =
+      "Invalid BYUI email address. Please enter a valid BYUI email address.";
+    email.style.backgroundColor = "rgb(201, 120, 120)";
+  }
+}
